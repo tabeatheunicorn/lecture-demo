a83fb1e30fbadfd9364ed5d6c4e77175674bff7f
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2025-07-01 10:11:01 +0200

    add polling for objects
    Store current response in signal to help with reactivity

--- src/app/objects-feature/api.service.ts
+++ src/app/objects-feature/api.service.ts
@@ -1,6 +1,6 @@
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, interval, switchMap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { ApiObject } from './object.model';
 
@@ -10,6 +10,23 @@ import { ApiObject } from './object.model';
 export class ApiService {
   private readonly baseUrl = `https://${environment.serverURL}`;
   private readonly http = inject(HttpClient);
+
+  private readonly apiObjects = signal<ApiObject[]>([]);
+  readonly objects = this.apiObjects.asReadonly();
+
+  constructor() {
+    // Poll every 5 seconds and update the signal
+    interval(5000)
+      .pipe(switchMap(() => this.getObjects()))
+      .subscribe({
+        next: (objects) => {
+          this.apiObjects.set(objects);
+        },
+        error: (err) => {
+          console.error('Error fetching objects:', err);
+        },
+      });
+  }
 
   getObjects(): Observable<ApiObject[]> {
     return this.http.get<ApiObject[]>(`${this.baseUrl}/object_list`);
