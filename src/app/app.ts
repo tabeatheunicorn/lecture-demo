ecc0a632e39fc357a10bb42e33cbd90089b10fb8
Tabea Röthemeyer <tabea.roethemeyer@gruppe.ai>
2025-07-01 12:09:26 +0200

    fixup! add ui and method to add object
    include component in app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ObjectsOverview } from "./objects-feature/objects-overview/objects-overview";
+import { GenericObjectAdd } from "./objects-feature/generic-object-add/generic-object-add";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ObjectsOverview],
+  imports: [RouterOutlet, ObjectsOverview, GenericObjectAdd],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
